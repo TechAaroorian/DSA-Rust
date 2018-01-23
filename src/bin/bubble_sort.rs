3a4a5dd91ab1796306ea3267e87aee14c46a92cf
janarthanan <janarthanan@onedatasoftware.com>
2018-01-23 17:50:14 +0530

    added comments for Bubble Sort file

--- src/bin/bubble_sort.rs
+++ src/bin/bubble_sort.rs
@@ -1,13 +1,45 @@
+/* Bubble Sort Algorithm in Rust
+* 
+* Bubble Sort
+* ------ ----
+*
+* -------------------------------------------
+*   let num_list_len = num_list.len();
+*   let mut got_swap;
+*   let mut i;
+*  
+*   loop {
+*       i = 0;
+*       got_swap = false;
+*       while i < num_list_len - 1 {
+*          if num_list[i] > num_list[i + 1] {
+*               let temp = num_list[i];
+*               num_list[i] = num_list[i + 1];
+*               num_list[i + 1] = temp;
+*               got_swap = true;
+*          }
+*           i += 1;
+*       }
+*       if !got_swap {
+*           break;
+*       }
+*   }
+* --------------------------------------------
+*
+*/
+
 use std::io;
 
 fn main() {
     let mut user_input = String::new();
 
-    println!("Please enter number to sort:");
+    println!("\nPlease enter numbers to sort:");
 
+    // Getting user input 
     io::stdin().read_line(&mut user_input)
         .expect("Failed to read input");
 
+    // Splitting and parsing the string to integers in vector
     let mut num_list: Vec<i32> = user_input.split_whitespace()
         .filter_map( |x| x.parse().ok() )
         .collect();
@@ -18,8 +50,10 @@ fn main() {
     let mut phase_count = 1;
 
     println!();
+
+    // Loop until sorting is finish
     loop {
-        println!("Phase {}:", phase_count);
+        println!("Pass {}:", phase_count);
         phase_count += 1;
         i = 0;
         got_swap = false;
@@ -34,6 +68,8 @@ fn main() {
             println!("{:?}", num_list);
         }
         println!();
+
+        // Checking for further sort; if fails break the loop 
         if !got_swap {
             break;
         }
